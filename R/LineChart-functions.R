548046ba6b9db0d40ca2393e0f792630c9927751
Kyle Hardman <kylehardman@gmail.com>
2014-08-28 12:49:23 -0500

    Added multi-group plotting. Played with documentation. Added "add" argument to vignette.

--- R/LineChart-functions.R
+++ R/LineChart-functions.R
@@ -1,106 +1,18 @@
 
-getSettingsDfCOlumnNames = function() {
-  c("group", "lty", "lwd", "symbol", "cex.symbol", "width.errBar", "color", "fillColor", "include")
-}
-
-replaceMissingSettings = function(settings) {
-  gsn = getSettingsDfCOlumnNames()
-  currentNames = names(settings)
-  
-  missing = gsn[!(gsn %in% currentNames)]
-  
-  if (length(missing) > 0) {
-    defaults = buildGroupSettings(settings$group, suppressWarnings=TRUE)
-    
-    for (m in missing) {
-      settings[,m] = defaults[,m]
-    }
-  }
-  
-  settings
-}
-
-extendSettings = function(settings, group) {
-  allGroups = unique(c(settings$group, group))
-  newSettings = buildGroupSettings(allGroups, suppressWarnings=TRUE)
-  
-  for (g in allGroups) {
-    if (g %in% settings$group) {
-      newSettings[g == newSettings$group,] = settings[g == settings$group,]
-    }
-  }
-  newSettings
-}
-
-
-#This can take a confidence interval (CI; two values around a central mean) or a mean and the amount
-#to go in either direction. Single-sided error bars can be made by providing one side of the CI and the mean. For asymmetrical or single-sided error bars, both CI and mean must be provided.
-drawErrorBars = function(x, CI=c(mean-ciAmount, mean+ciAmount), width=.1*par()$cex, ciAmount=NULL, mean=NULL, color="black", lty=1, lwd=1, vertical=TRUE) {
-  
-  x0 = rep(x, 2)
-  x1 = x0
-  y0 = rep(ifelse(is.null(mean), mean(CI), mean), 2)
-  y1 = CI[!is.null(CI)]
-  
-  if (!vertical) {
-    x0 = y0
-    x1 = y1
-    y0 = x0
-    y1 = x1
-  }
-
-  dSquared = (x0 - x1)^2 + (y0 - y1)^2
-  
-  for (i in 1:length(x0)) {
-    #Only draw arrows with nonzero length so as to avoid warnings
-    if (dSquared[i] != 0) {
-      arrows(x0=x0[i], x1=x1[i], y0=y0[i], y1=y1[i], angle=90, length=width, 
-             lty=lty, lwd=lwd, col=color, code=2)
-    }
-  }
-}
-
-#' Draw points with connect lines.
-#' 
-#' Draw points specified by a plotting data frame with connect lines. This function is primarily for internal use within the LineChart package. If you want to add connected points to a plot, it will probably be easier to use lineChart with add = TRUE.
-#'
-#' @param plotDf The data frame containing the data to be plotted. it must have several columns with specific names. See the return value of `createPlottingDf` for those column names.
-#' 
-#' @export
-drawConnectedPointsDf = function(plotDf) {
-  for (g in unique(plotDf$group)) {
-    pl = plotDf[plotDf$group == g,]
-    
-    if (pl$include[1]) {
-      lines(pl$x, pl$y, col=pl$color, lwd=pl$lwd, lty=pl$lty)
-      points(pl$x, pl$y, pch=pl$symbol, 
-             bg=pl$fillColor, col=pl$color,
-             cex=pl$cex.symbol, lwd=pl$lwd)
-    }
-  }
-}
-
 #' Plot data with a line chart
 #' 
 #' This function is the primary interface to the LineChart package.
 #' 
-#' @param formula A formula of the form "y ~ x * group", where "y", "x", and "group" are names of columns in `data`.
-#' @param data A data frame containing the data to be plotted.
+#' @inheritParams createPlottingDf
+#' @inheritParams lineChartDf
+#' 
 #' @param legendPosition The position at which the legend should be placed. If this is not NULL, it is passed through to legend directly.
-#' @param settings A data frame containing the appearance settings, such as returned by buildGroupSettings.
-#' @param errBarType The type of error bars to use. Can be one of "SE", "SD", or "CI95" for standard error, standard deviation, or 95\% confidence interval, respectively.
-#' @param title The title to place at the top of the plot.
-#' @param xlab The label to place on the x-axis. If NULL (default), the name of the source of data for the x-variable will be used.
-#' @param ylab The label to place on the y-axis. If NULL (default), the name of the source of data for the y-variable will be used.
 #' @param legendTitle The title to be used in the legend box. If "GROUP_NAME" (default), the name of the source of data for the grouping variable will be used. If no legend title is desired, use NULL.
-#' @param xlim The plotting range of the x-axis.
-#' @param ylim The plotting range of the y-axis.
-#' @param plotXAxis Should the x-axis be plotted?
-#' @param plotYAxis Should the y-axis be plotted?
-#' @param lwd.axes The line width of the axes and box around the plotting area.
-#' @param add If FALSE, a new plot will be started and the data plotted in it. If TRUE, the data will be plotted in the current plotting device, if any.
+#' 
 #' 
 #' @return The plotting data frame that was used is returned invisibly.
+#' 
+#' @seealso \code{\link{createPlottingDf}}, \code{\link{lineChartDf}}, \code{\link{buildGroupSettings}}, \code{\link{extractGroupSettings}}, \code{\link{applySettingsToPlottingDf}}, \code{\link{legendFromPlottingDf}}, \code{\link{legendFromSettings}}
 #' 
 #' @export
 #' 
@@ -152,6 +64,119 @@ lineChart = function(formula, data, legendPosition="topright", settings=NULL,
 }
 
 
+#' Creates a plottable data frame.
+#' 
+#' This function creates a data frame that can be used for plotting
+#' from a user-supplied data frame. The user data will be aggregated
+#' based on the formula. The resulting plotting data frame can be used 
+#' with `lineChartDf`.
+#' 
+#' Note that if no grouping variables are supplied by the formula, a `group` column 
+#' will still be made in the resulting data frame and the group name will be the number 0.
+#' 
+#' @param formula The formula should be of the form `y ~ x` or `y ~ x * g1 * g2 * ... * gK`, where the `gN` are any number of grouping variables and 'y', 'x', and 'gi' are the names of columns in `data`. If grouping variables are used, `x` must come before them.
+#' @param data A data frame containing the data to be used in creation of the plotting data frame.
+#' @param settings Plotting settings for the different groups in the data, such as the symbol to use.
+#' @param errBarType The type of error bar to use. Can be "SE" for standard error, "SD" for standard deviation, or "CI95" for a 95\% confidence interval. If NULL, no error bars are created. If a function is supplied, the function should take one argument, which will be the data used to plot a single data point, and return two error bar amounts. These amounts should be the signed difference between the end of the error bar and the mean of the data for that data point.
+#' @return A data frame which can be plotted with lineChartDf. See the documentation for lineChartDf for examples.
+#' @export
+#' 
+#' @examples
+#' 
+#' data(ChickWeight)
+#' 
+#' #Example of a user-defined error bar function:
+#' quartiles = function(x) {
+#'  as.numeric(quantile(x - mean(x), c(0.25, 0.75)))
+#' }
+#' 
+#' plotDf = createPlottingDf(weight ~ Time * Diet, ChickWeight, errBarType=quartiles)
+#' lineChartDf(plotDf)
+#' 
+createPlottingDf = function(formula, data, settings = NULL, errBarType = "SE") {
+  
+  mf = model.frame(formula=formula, data=data)
+  
+  plotDf = aggregate(formula, mf, mean)
+  
+  
+  terms = terms.formula(formula)
+  
+  groups = as.character(attr(terms, "variables"))
+  
+  groupCount = length(groups) - 3
+  
+  hasGroups = groupCount > 0
+  
+  if (hasGroups) {
+    groups = groups[ 4:length(groups) ]
+    
+    levels = NULL
+    if (groupCount > 1) {
+      levels = apply(plotDf[ , groups ], 1, function(x) { paste(x, sep="", collapse=":") })
+    } else {
+      levels = plotDf[ , groups ]
+    }
+    
+    plotDf = plotDf[ , !(names(plotDf) %in% groups)]
+    
+    plotDf[ , paste(groups, collapse=":") ] = levels
+    
+  } else {
+    plotDf[ , "indicator_group" ] = 0
+  }
+  
+  attr(plotDf, "originalNames") = list(y = names(plotDf)[2],
+                                       x = names(plotDf)[1],
+                                       group = names(plotDf)[3]
+  )
+  if (!hasGroups) {
+    attr(plotDf, "originalNames")$group = NULL
+  }
+  names(plotDf) = c("x", "y", "group")
+  
+  
+  errorBarFunction = NULL
+  if (is.function(errBarType)) {
+    errorBarFunction = errBarType
+  } else {
+    if (!is.null(errBarType)) {
+      if (errBarType == "SE") {
+        errorBarFunction = function(x) { y = sqrt(var(x)/length(x)); c(-y, y) }
+      } else if (errBarType == "CI95") {
+        errorBarFunction = function(x) {
+          t = t.test(x, conf.level=.95)
+          as.numeric(t$conf.int - t$estimate)
+        }
+      } else if (errBarType == "SD") {
+        errorBarFunction = function(x) { y = sqrt(var(x)); c(-y, y) }
+      }
+    }
+  }
+  
+  if (is.null(errorBarFunction)) {
+    plotDf$errBar = NA
+    plotDf$errBarLower = NA
+  } else {
+    #plotDf$errBar = aggregate(formula, mf, errorBarFunction)[,names(mf)[1]]
+    
+    errBars = aggregate(formula, mf, errorBarFunction)[,names(mf)[1]]
+    
+    errBars[ errBars[,1] > errBars[,2], c(1,2) ] = errBars[ errBars[,1] > errBars[,2], c(2,1) ]
+    
+    plotDf$errBarLower = errBars[,1]
+    plotDf$errBar = errBars[,2]
+  }
+  
+  if (is.null(settings)) {
+    settings = buildGroupSettings(plotDf$group, suppressWarnings=TRUE)
+  }
+  plotDf = applySettingsToPlottingDf(settings, plotDf)
+  
+  plotDf
+}
+
+
 #' Plot data using a plotting data frame
 #' 
 #' This function uses a data frame with the same format as one created by createPlottingDf.
@@ -163,7 +188,7 @@ lineChart = function(formula, data, legendPosition="topright", settings=NULL,
 #' If xlab and/or ylab are NULL, the original names of the source of the data are used for plotting, if available. The original names are stored in attributes and sometimes attributes are lost or, in the case of a manually created plotting data frame, not present. The original names are stored in an attribute called "originalNames" which is a list with "y", "x", and "group" components.
 #' 
 #' @param plotDf The data frame to plotted.
-#' 
+#'
 #' @param title The title to place at the top of the plot.
 #' @param xlab The label to place on the x-axis. If NULL (default), the name of the source of data for the x-variable will be used, if available.
 #' @param ylab The label to place on the y-axis. If NULL (default), the name of the source of data for the y-variable will be used if available.
@@ -428,80 +453,6 @@ applySettingsToPlottingDf = function (settings, plotDf) {
   plotDf
 }
 
-
-#' Creates a plottable data frame.
-#' 
-#' This function creates a data frame that can be used for plotting
-#' from a user-supplied data frame. The user data will be aggregated
-#' based on the formula. The resulting plotting data frame can be used 
-#' with `lineChartDf`.
-#' 
-#' @param formula The formula should be of the form `y ~ x * group` or `y ~ x`. `x` must come before `group`.
-#' @param data A data frame containing the data to be plotted.
-#' @param settings Plotting settings for the different groups in the data, such as the symbol to use.
-#' @param errBarType String. The type of error bar to use. Can be "SE" for standard error, "SD" for standard deviation, or "CI95" for a 95\% confidence interval. If NULL, no error bars are created.
-#' @return A data frame which can be plotted with lineChartDf. See the documentation for lineChartDf for examples.
-#' @export
-createPlottingDf = function(formula, data, settings = NULL, errBarType = "SE") {
-  
-  mf = model.frame(formula=formula, data=data)
-  
-  plotDf = aggregate(formula, mf, mean)
-  
-  if (ncol(mf) == 3) {
-    attr(plotDf, "originalNames") = list(x = names(mf)[2],
-                                         group = names(mf)[3],
-                                         y = names(mf)[1])
-    names(plotDf) = c("x", "group", "y")
-  } else if (ncol(mf) == 2) {
-    attr(plotDf, "originalNames") = list(x = names(mf)[2],
-                                         group = NULL,
-                                         y = names(mf)[1])
-    names(plotDf) = c("x", "y")
-    plotDf$group = "1"
-  }
-  
-  #attr(plotDf, "originalNames") = list(x = names(mf)[2],
-  #                                     group = names(mf)[3],
-  #                                     y = names(mf)[1])
-  #names(plotDf) = c("x", "group", "y")
-  
-  errorBarFunction = NULL
-  
-  if (!is.null(errBarType)) {
-    if (errBarType == "SE") {
-      errorBarFunction = function(x) { sqrt(var(x)/length(x)) }
-    } else if (errBarType == "CI95") {
-      errorBarFunction = function(x) {
-        t = t.test(x, conf.level=.95)
-        as.numeric(t$conf.int[2] - t$estimate)
-      }
-    } else if (errBarType == "SD") {
-      errorBarFunction = function(x) { sqrt(var(x)) }
-    }
-  }
-  
-  if (is.null(errorBarFunction)) {
-    plotDf$errBar = NA
-    plotDf$errBarLower = NA
-  } else {
-    #plotDf$errBar = aggregate(formula, mf, errorBarFunction)[,names(mf)[1]]
-    
-    absErrBar = aggregate(formula, mf, errorBarFunction)[,names(mf)[1]]
-    
-    plotDf$errBar = absErrBar
-    plotDf$errBarLower = -absErrBar
-  }
-  
-  if (is.null(settings)) {
-    settings = buildGroupSettings(plotDf$group, suppressWarnings=TRUE)
-  }
-  plotDf = applySettingsToPlottingDf(settings, plotDf)
-  
-  plotDf
-}
-
-
 #' Create a legend based on a plotting data frame with group appearance settings
 #' 
 #' @param position The position of the legend. This is passed to the `x` argument of legend().
@@ -532,4 +483,85 @@ legendFromSettings = function(position, settings, ...) {
          title = vargs$title, 
          horiz = ifelse(is.null(vargs$horiz), FALSE, vargs$horiz)  
   )
+}
+
+
+
+getSettingsDfCOlumnNames = function() {
+  c("group", "lty", "lwd", "symbol", "cex.symbol", "width.errBar", "color", "fillColor", "include")
+}
+
+replaceMissingSettings = function(settings) {
+  gsn = getSettingsDfCOlumnNames()
+  currentNames = names(settings)
+  
+  missing = gsn[!(gsn %in% currentNames)]
+  
+  if (length(missing) > 0) {
+    defaults = buildGroupSettings(settings$group, suppressWarnings=TRUE)
+    
+    for (m in missing) {
+      settings[,m] = defaults[,m]
+    }
+  }
+  
+  settings
+}
+
+extendSettings = function(settings, group) {
+  allGroups = unique(c(settings$group, group))
+  newSettings = buildGroupSettings(allGroups, suppressWarnings=TRUE)
+  
+  for (g in allGroups) {
+    if (g %in% settings$group) {
+      newSettings[g == newSettings$group,] = settings[g == settings$group,]
+    }
+  }
+  newSettings
+}
+
+
+#This can take a confidence interval (CI; two values around a central mean) or a mean and the amount
+#to go in either direction. Single-sided error bars can be made by providing one side of the CI and the mean. For asymmetrical or single-sided error bars, both CI and mean must be provided.
+drawErrorBars = function(x, CI=c(mean-ciAmount, mean+ciAmount), width=.1*par()$cex, ciAmount=NULL, mean=NULL, color="black", lty=1, lwd=1, vertical=TRUE) {
+  
+  x0 = rep(x, 2)
+  x1 = x0
+  y0 = rep(ifelse(is.null(mean), mean(CI), mean), 2)
+  y1 = CI[!is.null(CI)]
+  
+  if (!vertical) {
+    x0 = y0
+    x1 = y1
+    y0 = x0
+    y1 = x1
+  }
+  
+  dSquared = (x0 - x1)^2 + (y0 - y1)^2
+  
+  for (i in 1:length(x0)) {
+    #Only draw arrows with nonzero length so as to avoid warnings
+    if (dSquared[i] != 0) {
+      arrows(x0=x0[i], x1=x1[i], y0=y0[i], y1=y1[i], angle=90, length=width, 
+             lty=lty, lwd=lwd, col=color, code=2)
+    }
+  }
+}
+
+#' Draw points with connect lines.
+#' 
+#' Draw points specified by a plotting data frame with connect lines. This function is primarily for internal use within the LineChart package. If you want to add connected points to a plot, it will probably be easier to use lineChart with add = TRUE.
+#'
+#' @param plotDf The data frame containing the data to be plotted. it must have several columns with specific names. See the return value of `createPlottingDf` for those column names.
+drawConnectedPointsDf = function(plotDf) {
+  for (g in unique(plotDf$group)) {
+    pl = plotDf[plotDf$group == g,]
+    
+    if (pl$include[1]) {
+      lines(pl$x, pl$y, col=pl$color, lwd=pl$lwd, lty=pl$lty)
+      points(pl$x, pl$y, pch=pl$symbol, 
+             bg=pl$fillColor, col=pl$color,
+             cex=pl$cex.symbol, lwd=pl$lwd)
+    }
+  }
 }
